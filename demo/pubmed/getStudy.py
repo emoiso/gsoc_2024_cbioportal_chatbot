be2f2102aed3f6b86d4c8caf92b66defbef8614e
XinlingWang0628 <105673895+XinlingWang0628@users.noreply.github.com>
2024-06-24 16:09:37 -0400

    Update getStudy.py

--- demo/pubmed/getStudy.py
+++ demo/pubmed/getStudy.py
@@ -34,16 +34,16 @@ def read_and_extract_xml_data(fileName):
 
 
 pmid_list, no_pmid_list, pmcid_list, no_xml, pmcid_no_xml = [], [], [], [], []
-allStudy = load_json_file('pubmed/cBioportal_study.json')
+allStudy = load_json_file('pubmed/cBioportal_study.json')           # all study =411
 
 # FIRSR STEP
 # make a list for studies have pmid and a list without pmid
 for study in allStudy:
     pmid_value = study.get('pmid')
     if pmid_value:  
-        pmid_list.append(pmid_value)
+        pmid_list.append(pmid_value)                                #pmid = 351
     else:  # some study have no pmid
-        no_pmid_list.append(study)
+        no_pmid_list.append(study)                                  # no pmid = 60
 
 print(len(pmid_list))
 print(len(no_pmid_list))
@@ -51,33 +51,52 @@ print(len(no_pmid_list))
 
 
 # SECOND STEP
-# # convert pmcid from pmid, then write a file for pmcid
+# # convert pmcid from pmid, then write a file for pmcid            #pmcid = 351
 # pmcid_list = convert_pmcid_from_pmid()
-# write_json_file('pmcid_list.json',pmcid_list)
+# write_json_file('pmcid_list.json', pmcid_list)
 pmcid_data = load_json_file('pubmed/pmcid_list.json')
 
 
 # THIRD STEP
 # download xml file using pmcids, and extract content from xml papers
-for study in pmcid_data:
+i = 0
+j = 0
+k = 0
+for study in pmcid_data:                                            # 351
     pmcid = study.get('pmcid')
     try: 
-        if pmcid:
-            # download xml file using aws S3
-            download_pmc_s3(pmcid)
-            fileName = str('pmc/' + pmcid + '.xml')
-            # read and extract xml data
-            text = read_and_extract_xml_data(fileName)
-            # write a txt file for extract xml content
-            with open('loaded_pmc/' + pmcid + '.txt', 'w') as f:
-                f.write(str(text))
+        if pmcid:  
+            file_path = f'loaded_pmc/{pmcid}.txt'
+            if not os.path.exists(file_path):
+                # download xml file using aws S3
+                try: 
+                    download_pmc_s3(pmcid)
+                    fileName = str('pmc/' + pmcid + '.xml')
+                    try:
+                        # read and extract xml data
+                        text = read_and_extract_xml_data(fileName)
+                        # write a txt file for extract xml content
+                        with open('loaded_pmc/' + pmcid + '.txt', 'w') as f:
+                            f.write(str(text))
+                    except Exception as e:                                  #29
+                        j += 1
+                        print(f"Error read and extract pmcid {pmcid}: {e}")
 
-        else:  # pmcid = null
+                except Exception as e: 
+                    k += 0
+                    print(f"can't be downloaded {pmcid}: {e}")
+
+        else:  # pmcid = null                                           #40
             no_xml.append(study)
 
-    except Exception:  # study has pmcid, but no xml form
+    except Exception as e:  # study has pmcid, but no xml form
+        i += 1
+        print(f"Error downloading pmcid {pmcid}: {e}")
         pmcid_no_xml.append(study)
         print("This study" + pmcid + " has pmcid, but no xml paper")
 
+print("failed to download file number: " + str(i))
+print("failed to extract and load file number: " + str(j))
+print("total cannot be downloaded: " + str(k))
 write_json_file('no_xml.json', no_xml)  
 write_json_file('pmcid_no_xml.json', pmcid_no_xml)
