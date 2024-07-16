dd9461e2c40e4abb61ef3642f5b5648eb5b7427d
XinlingWang00 <Xinlingwang0628@gmail.com>
2024-07-16 18:02:56 -0400

    update pubmed chatbot

--- demo/pubmed/getStudy.py
+++ demo/pubmed/getStudy.py
@@ -1,3 +1,110 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:d0c5002bc735f4130322d167849691cf1e7f077ede539be22001a97d089f5883
-size 3597
+import json 
+from data.indra_nxml_extraction import id_lookup
+from data.download_pmc_s3 import download_pmc_s3 
+import os
+from data.indra_nxml_extraction import get_xml, get_xml_from_file, extract_text
+from lxml import etree
+from bs4 import BeautifulSoup
+
+
+def load_json_file(path):
+    with open(path) as f:
+        data = json.load(f)
+    return data    
+
+
+def write_json_file(fileName, data):
+    with open(fileName, 'w', encoding='utf-8') as f:
+        json.dump(data, f, ensure_ascii=False, indent=4)
+
+
+def convert_pmcid_from_pmid():
+    for pmid in pmid_list:
+        pmcid = id_lookup(pmid)
+        pmcid_list.append(pmcid)
+    return pmcid_list
+
+
+def read_and_extract_xml_data(fileName):
+    with open(fileName, 'r') as f:
+        data = f.read()
+    xml_data = BeautifulSoup(data, "xml")
+    text = extract_text(xml_data)
+    return text
+
+
+pmid_list, no_pmid_list, pmcid_list, no_xml, pmcid_no_xml = set(), [], [], [], []
+allStudy = load_json_file('pubmed/cBioportal_study.json')          
+
+# FIRSR STEP
+# make a list for studies have pmid and a list without pmid
+for study in allStudy:
+    pmid_value = study.get('pmid')
+    if pmid_value:  # some studies have multi pmids
+        print(pmid_value)
+        if len(pmid_value) > 8:
+            curr = pmid_value.split(',')
+            print(curr)
+            for id in curr:   
+                pmid_list.add(id)
+        else: 
+            pmid_list.add(pmid_value)        
+    else:  # some study have no pmid
+        no_pmid_list.append(study)    
+
+print(pmid_list)
+print(len(pmid_list))
+print(len(no_pmid_list))
+# write_json_file('no_pmid.json',no_pmid_list)
+
+
+# SECOND STEP
+# convert pmcid from pmid, then write a file for pmcid
+pmcid_list = convert_pmcid_from_pmid()
+write_json_file('pubmed/pmcid_list.json', pmcid_list)
+pmcid_data = load_json_file('pubmed/pmcid_list.json')
+
+
+# THIRD STEP
+# download xml file using pmcids, and extract content from xml papers
+i = 0
+j = 0
+k = 0
+for study in pmcid_data:                                            # 351
+    pmcid = study.get('pmcid')
+    try: 
+        if pmcid:  
+            file_path = f'loaded_pmc/{pmcid}.txt'
+            if not os.path.exists(file_path):
+                # download xml file using aws S3
+                try: 
+                    download_pmc_s3(pmcid)
+                    fileName = str('pmc/' + pmcid + '.xml')
+                    try:
+                        # read and extract xml data
+                        text = read_and_extract_xml_data(fileName)
+                        # write a txt file for extract xml content
+                        with open('loaded_pmc/' + pmcid + '.txt', 'w') as f:
+                            f.write(str(text))
+                    except Exception as e:                                  #29
+                        j += 1
+                        print(f"Error read and extract pmcid {pmcid}: {e}")
+
+                except Exception as e: 
+                    k += 0
+                    print(f"can't be downloaded {pmcid}: {e}")
+
+        else:  # pmcid = null                                           #40
+            no_xml.append(study)
+
+    except Exception as e:  # study has pmcid, but no xml form
+        i += 1
+        print(f"Error downloading pmcid {pmcid}: {e}")
+        pmcid_no_xml.append(study)
+        print("This study" + pmcid + " has pmcid, but no xml paper")
+
+print("failed to download file number: " + str(i))
+print("failed to extract and load file number: " + str(j))
+print("total cannot be downloaded: " + str(k))
+write_json_file('no_xml.json', no_xml)  
+write_json_file('pmcid_no_xml.json', pmcid_no_xml)
--- demo/pubmed/pubmed_demo.py
+++ demo/pubmed/pubmed_demo.py
@@ -1,3 +1,391 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:94a6c00a161f2aaa37a6b1e51d47d6e54139aa1a7e1f7c294eb6dfdb78ba29f9
-size 12526
+from langchain.document_loaders.base import BaseLoader
+from pathlib import Path
+from typing import Dict, List, Union
+from langchain.docstore.document import Document
+import gradio as gr
+from dotenv import load_dotenv, find_dotenv
+from langchain_community.vectorstores import Chroma
+from langchain_core.messages import HumanMessage, AIMessage
+from langchain_openai import AzureChatOpenAI
+from langchain_openai import AzureOpenAIEmbeddings
+from langchain_core.prompts import ChatPromptTemplate
+import os
+from langchain_core.runnables import RunnablePassthrough 
+from langchain_core.output_parsers import StrOutputParser
+from typing import Iterator
+# from langchain_community.document_loaders import UnstructuredFileLoader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.schema.document import Document
+from langchain_community.document_loaders import DirectoryLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.retrievers.self_query.base import SelfQueryRetriever
+from langchain.chains.query_constructor.base import AttributeInfo
+import json
+from collections import defaultdict
+from langchain_community.document_loaders import PyMuPDFLoader
+import chromadb
+import time
+
+#langchain-core==0.2.3
+def load_json_file(path):
+    with open(path) as f:
+        data = json.load(f)
+    return data  
+
+
+pmid_data = load_json_file('demo/pubmed/data/pmcid_list.json')
+study_data = load_json_file('demo/pubmed/data/cBioportal_study.json')
+pmid_dict = {}
+study_dict = defaultdict(list)
+
+# pair pmid and pmcid in dict
+for data in pmid_data:
+    pmcid = data.get('pmcid')
+    if pmcid:
+        pmid = data.get('pmid')
+        pmid_dict[pmcid] = pmid
+
+
+for study in study_data:
+    pmid = study.get('pmid')
+    if pmid:
+        pmid = pmid.strip()
+        if len(pmid) > 8:  # more than one pmid
+            curr = pmid.split(',')
+            for id in curr:
+                study_dict[id].append(study)
+        else:
+            study_dict[pmid].append(study)
+
+
+class pubmedLoader(BaseLoader):
+    def __init__(
+        self,
+        file_path: Union[str, Path],
+
+    ):
+        self.file_path = Path(file_path).resolve()
+
+    def load(self) -> List[Document]:  
+        """Load and return documents from the pubmed file."""
+        docs: List[Document] = []
+
+        with open(self.file_path, encoding="utf-8") as f:
+            pubmed_content = f.read()
+        # extract paper title 
+        with open(self.file_path, encoding="utf-8") as file:
+            title = file.readline().strip()
+
+        text_splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(
+            chunk_size=512, 
+            chunk_overlap=256,
+            separators=["\n", "Results", "Discussion", "Method Summary", "Tissue Source Sites", "Supplementary Material"]
+        )        
+        pubmed_splits = text_splitter.split_text(pubmed_content)
+        #convert string to doc, otherwise it cannot have metaData
+        pubmed_splits_doc = text_splitter.create_documents(pubmed_splits)
+        for split in pubmed_splits_doc:
+            docs.append(split)
+
+        #extract pmcid from file name, and get pmid & study content
+        pmcid = Path(self.file_path).stem
+        pmid = pmid_dict[pmcid]
+        study = {}
+        if pmid in study_dict:
+            study = study_dict[pmid]
+        if len(study) > 1:  # for one paper used in multi-study
+            i = 2
+            # Process each study and store it in the study_dict
+            content_dict = {}
+            for s in study:
+                for key, value in s.items():
+                    if key in content_dict:
+                        content_dict[(key + str(i))] = value
+                    else:
+                        content_dict[key] = value
+                i += 1        
+            study_dict[pmid] = content_dict
+            study = study_dict[pmid]
+        else:
+            study = study[0]
+
+        # add metaData
+        for doc in docs:
+            doc.metadata['pmc_id'] = pmcid
+            doc.metadata['paper_title'] = title
+            for k, v in study.items():
+                doc.metadata[k] = v          
+            doc.metadata['pmid'] = pmid    
+        return docs
+
+
+class PDFLoader(BaseLoader):
+    def __init__(
+        self,
+        file_path: Union[str, Path]
+    ):
+
+        self.file_path = Path(file_path).resolve()
+
+    def load(self) -> List[Document]:
+        """Load and return documents from the PDF file."""
+        docs: List[Document] = []
+
+        loader = PyMuPDFLoader(self.file_path)
+        pages = loader.load()
+        # remove default metadata
+        for page in pages:  
+            page.metadata.clear()
+
+        pmcid = Path(self.file_path).stem
+        pmid = pmid_dict[pmcid]
+        study = {}
+        if pmid in study_dict:
+            study = study_dict[pmid]
+
+        if len(study) > 1:  # for one paper used in multi-study
+            i = 2
+            # Process each study and store it in the study_dict
+            content_dict = {}
+            for s in study:
+                for key, value in s.items():
+                    if key in content_dict:
+                        content_dict[(key + str(i))] = value
+                    else:
+                        content_dict[key] = value
+                i += 1        
+            study_dict[pmid] = content_dict
+            study = study_dict[pmid]
+        else:
+            study = study[0]
+        # add metaData
+        for page in pages:
+            page.metadata['pmc_id'] = pmcid
+            # doc.metadata['paper_title'] = title
+            for k, v in study.items():
+                page.metadata[k] = v          
+            page.metadata['pmid'] = pmid 
+
+        text_splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(
+            chunk_size=512, 
+            chunk_overlap=256,
+            separators=["\n", "Results", "Analysis", "Discussion", "Method Summary", "Tissue Source Sites", "Supplementary Material"]
+        )        
+        splits = text_splitter.split_text(str(pages))
+        splits_doc = text_splitter.create_documents(splits)
+        for split in splits_doc:
+            docs.append(split)
+
+        return docs
+
+
+def load_docs(dir, loader):
+    loader = DirectoryLoader(path=dir, loader_cls=loader, show_progress=True)
+    docs = loader.load()
+    print(len(docs))
+    with open(f"{dir}.txt", 'w') as f:
+        f.write(str(docs))
+    return docs
+
+
+# docs = load_docs('demo/pubmed/pdf', PDFLoader)
+# print(len(docs))
+
+load_dotenv()
+_ = load_dotenv(find_dotenv())  # read local .env file
+
+#Azure_OpenAI llm    
+llm_azure = AzureChatOpenAI(
+    azure_endpoint=os.environ["AZURE_OPENAI_ENDPOINT"], 
+    openai_api_key=os.environ["AZURE_OPENAI_API_KEY"],
+    openai_api_version=os.environ["OPENAI_API_VERSION"],
+    deployment_name=os.environ["DEPLOYMENT_NAME"],
+    openai_api_type=os.environ["OPENAI_API_TYPE"]
+)
+
+embeddings = AzureOpenAIEmbeddings(
+    azure_deployment=os.environ["DEPLOYMENT_NAME_EMBEDDING"],
+    openai_api_version=os.environ["OPENAI_API_VERSION"],
+    azure_endpoint=os.environ["AZURE_OPENAI_ENDPOINT"],
+    api_key=os.environ["AZURE_OPENAI_API_KEY"]
+)
+
+# # first part of embeddings
+# vectordb_total = Chroma.from_documents(
+#     documents=docs[:3],
+#     embedding=embeddings,
+#     persist_directory="vectordb/chroma/pubmed/pdf2"
+# )
+# vectordb_total.persist()
+
+
+def update_vectordb_with_docs(docs, embeddings, base_persist_directory):
+    # the db created for first part of db
+    vectordb_total = Chroma(persist_directory=base_persist_directory, embedding_function=embeddings) 
+
+    # Iterate through the documents and update the vectordb
+    for i in range(int(len(docs) / 3)):
+        start, end = i * 3, (i + 1) * 3
+        docs_to_embed = docs[start:end]
+        vectordb_new = Chroma.from_documents(
+            documents=docs_to_embed,
+            embedding=embeddings,
+            
+        )
+        
+        new_data = vectordb_new._collection.get(include=['documents', 'metadatas', 'embeddings'])
+        vectordb_total._collection.add(
+            embeddings=new_data['embeddings'],
+            metadatas=new_data['metadatas'],
+            documents=new_data['documents'],
+            ids=new_data['ids']
+        )
+        print(vectordb_total._collection.count())  
+    return vectordb_total
+
+
+# vectordb_total = update_vectordb_with_docs(docs, embeddings, "vectordb/chroma/pubmed/pdf2")
+vectordb_total = Chroma(persist_directory="demo/vectordb/chroma/pubmed/paper_and_pdf", embedding_function=embeddings)
+
+# retriever = vectordb.as_retriever(k=5)
+
+# self-query retriever
+document_content_description = "full text of Pubmed papers "
+metadata_field_info = [
+    AttributeInfo(
+        name="pmc_id",
+        description="The id for pubmed paper, start with PMC, useful to search",
+        type="string",
+    ),
+    AttributeInfo(
+        name="title",
+        description="The title for pubmed paper",
+        type="string",
+    ),
+    AttributeInfo(
+        name="name",
+        description="The name of study using this pubmed paper",
+        type="string",
+    ),
+    AttributeInfo(
+        name="description",
+        description="The description of study using this pubmed paper",
+        type="string",
+    ),
+    # AttributeInfo(
+    #     name="publicStudy",
+    #     description=" Whether the study is public",
+    #     type="boolean",
+    # ),
+    # AttributeInfo(
+    #     name="citation",
+    #     description="The citation of study using this pubmed paper",
+    #     type="string",
+    # ),
+    AttributeInfo(
+        name="groups",
+        description="The groups of study using this pubmed paper",
+        type="string",
+    ),
+    # AttributeInfo(
+    #     name="status",
+    #     description="The status of study using this pubmed paper",
+    #     type="int",
+    # ),
+    # AttributeInfo(
+    #     name="importDate",
+    #     description="The importDate of study using this pubmed paper",
+    #     type="date",
+    # ),
+    AttributeInfo(
+        name="allSampleCount",
+        description="The allSampleCount of study using this pubmed paper",
+        type="int",
+    ),
+    # AttributeInfo(
+    #     name="readPermission",
+    #     description="The readPermission of study using this pubmed paper",
+    #     type="boolean",
+    # ),
+    AttributeInfo(
+        name="studyId",
+        description="The studyId of study using this pubmed paper",
+        type="string",
+    ),
+    # AttributeInfo(
+    #     name="cancerTypeId",
+    #     description="The cancerTypeId of study using this pubmed paper",
+    #     type="string",
+    # ),
+    # AttributeInfo(
+    #     name="referenceGenome",
+    #     description="The referenceGenome of study using this pubmed paper",
+    #     type="string",
+    # ),
+    AttributeInfo(
+        name="name",
+        description="The name of study using this pubmed paper",
+        type="string",
+    ),
+]
+SelfQuery_Retriever = SelfQueryRetriever.from_llm(
+    llm_azure,
+    vectordb_total,
+    document_content_description,
+    metadata_field_info
+)
+
+
+# build prompt template
+ANSWER_PROMPT = """You are a professional assistant. Answer questions using content & metadata.
+                Also, ignore the context if it is a reference.
+                Answer the question based only on the following context, 
+                and return the pmc_id, pmid, studyID of all the contexts :
+{context}
+Question: {question}
+"""
+
+prompt = ChatPromptTemplate.from_template(ANSWER_PROMPT)
+
+
+def get_pubmed_chain():
+    chain = (
+        {"context": SelfQuery_Retriever, "question": RunnablePassthrough()} 
+        | prompt  # choose a prompt
+        | llm_azure  # choose a llm
+        | StrOutputParser()
+    )
+    return chain
+
+
+# build a chain # lambda x:
+def get_response(question):
+    chain = get_pubmed_chain()
+    ans = chain.invoke(question)
+    return ans
+
+
+# # User Interface
+# def predict(message, history):
+#     history_langchain_format = []
+#     for human, ai in history:
+#         history_langchain_format.append(HumanMessage(content=human))
+#         history_langchain_format.append(AIMessage(content=ai))
+#     history_langchain_format.append(HumanMessage(content=message))
+#     gpt_response = get_response(message)
+#     return gpt_response
+
+
+# chatbot = gr.Chatbot(  # uploaded image of user and cBioportal as avatar 
+#     [],
+#     elem_id="chatbot",
+#     bubble_full_width=False,
+#     avatar_images=("demo/sample_data/user_avatar.png", 
+#                    "demo/sample_data/chatbot_avatar.png"),
+# )
+
+# gr.ChatInterface(
+#                  predict, 
+#                  title="cBioPortal pubmed ChatBot", 
+#                  examples=['What is PMC_ID PMC2671642 about?', 'What papers used in studyID acbc_mskcc_2015'],
+#                  chatbot=chatbot).launch()
