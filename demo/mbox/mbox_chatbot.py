ac4841c584eb2361888d2d7d3b1b9b4f95043848
XinlingWang00 <Xinlingwang0628@gmail.com>
2024-07-17 11:21:17 -0400

    update mbox chatbot

--- demo/mbox/mbox_chatbot.py
+++ demo/mbox/mbox_chatbot.py
@@ -1,3 +1,174 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:bb461c8608720b52ca93e6e362e377ff63a9485d0d02a761897056d21c77c340
-size 2999
+from langchain.document_loaders import DirectoryLoader
+import os
+from langchain_core.runnables import RunnablePassthrough 
+from langchain_core.output_parsers import StrOutputParser
+import gradio as gr
+from dotenv import load_dotenv, find_dotenv
+from langchain_community.vectorstores import Chroma
+from langchain_core.messages import HumanMessage, AIMessage
+from langchain_openai import AzureChatOpenAI
+from langchain_openai import AzureOpenAIEmbeddings
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_community.document_loaders import JSONLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+import json
+from langchain.retrievers.self_query.base import SelfQueryRetriever
+from langchain.chains.query_constructor.base import AttributeInfo
+
+
+load_dotenv()
+_ = load_dotenv(find_dotenv())  # read local .env file
+
+
+def load_and_split(path):
+    loader = JSONLoader(
+        file_path=path,
+        jq_schema=".",
+        text_content=False
+    )
+
+    data = loader.load()
+    text_splitter = RecursiveCharacterTextSplitter(
+        separators=["},", '\n'],
+        chunk_size=512,
+        chunk_overlap=200)
+    split_docs = text_splitter.split_documents(data)
+    return split_docs
+
+
+# split_docs = load_and_split('demo/mbox/deleted_error_mbox.json')
+# docs = []
+# for doc in split_docs:
+#     doc.metadata.clear()
+#     docs.append(doc)
+# print(len(docs))
+
+
+# def write_json_file(fileName, data):
+#     with open(fileName, 'w', encoding='utf-8') as f:
+#         json.dump(data, f, ensure_ascii=False, indent=4)
+
+
+# write_json_file('deleted_error_mbox.txt', str(docs))
+
+#Azure_OpenAI llm    
+llm_azure = AzureChatOpenAI(
+    azure_endpoint=os.environ["AZURE_OPENAI_ENDPOINT"], 
+    openai_api_key=os.environ["AZURE_OPENAI_API_KEY"],
+    openai_api_version=os.environ["OPENAI_API_VERSION"],
+    deployment_name=os.environ["DEPLOYMENT_NAME"],
+    openai_api_type=os.environ["OPENAI_API_TYPE"]
+)
+
+embeddings = AzureOpenAIEmbeddings(
+    azure_deployment=os.environ["DEPLOYMENT_NAME_EMBEDDING"],
+    openai_api_version=os.environ["OPENAI_API_VERSION"],
+    azure_endpoint=os.environ["AZURE_OPENAI_ENDPOINT"],
+    api_key=os.environ["AZURE_OPENAI_API_KEY"]
+)
+
+# # first part of embeddings
+# vectordb = Chroma.from_documents(
+#     documents=docs[:2],
+#     embedding=embeddings,
+#     persist_directory="demo/vectordb/chroma/mbox_07"
+# )
+# vectordb.persist()
+
+
+def update_vectordb_with_docs(docs, embeddings, base_persist_directory):
+    # the db created for first part of db
+    vectordb_total = Chroma(persist_directory=base_persist_directory, embedding_function=embeddings) 
+    # Iterate through the documents and update the vectordb
+    for i in range(len(docs) // 2):
+        start, end = 2+i * 2, 2+ 2 * (i + 1)
+        docs_to_embed = docs[start:end]
+        try:
+            vectordb_new = Chroma.from_documents(
+                documents=docs_to_embed,
+                embedding=embeddings
+            )
+            
+            new_data = vectordb_new._collection.get(include=['documents', 'metadatas', 'embeddings'])
+            vectordb_total._collection.add(
+                embeddings=new_data['embeddings'],
+                metadatas=new_data['metadatas'],
+                documents=new_data['documents'],
+                ids=new_data['ids']
+            )
+        except Exception as e:
+            print(e)
+            print(str(i) + " is too big")
+        print(vectordb_total._collection.count())  
+    return vectordb_total
+
+
+# vectordb = update_vectordb_with_docs(docs, embeddings, "demo/vectordb/chroma/mbox_07")
+vectordb = Chroma(persist_directory="demo/vectordb/chroma/mbox_no_err", embedding_function=embeddings)
+
+
+# self-query retriever
+document_content_description = "Google group conversations of cBioportal "
+metadata_field_info = [
+    AttributeInfo(
+        name="url",
+        description="The url for conversations",
+        type="string",
+    )
+]
+SelfQuery_Retriever = SelfQueryRetriever.from_llm(
+    llm_azure,
+    vectordb,
+    document_content_description,
+    metadata_field_info
+)
+
+
+retriever = vectordb.as_retriever(k=3)
+# build prompt template
+ANSWER_PROMPT = """Answer the question based only on the following context, 
+                Do not mention user name 
+                Return the url of google group conversation inside contexts :
+{context}
+Question: {question}
+"""
+
+prompt = ChatPromptTemplate.from_template(ANSWER_PROMPT)
+
+
+# build a chain # lambda x:
+def get_mbox_chain():
+    chain = (
+        {"context": SelfQuery_Retriever, "question": RunnablePassthrough()} 
+        | prompt  # choose a prompt
+        | llm_azure  # choose a llm
+        | StrOutputParser()
+    )
+    return chain
+
+
+# def getAnswer(question):
+#     ans = get_mbox_chain.invoke(question)
+#     return ans
+
+
+# # User Interface
+# def predict(message, history):
+#     history_langchain_format = []
+#     for human, ai in history:
+#         history_langchain_format.append(HumanMessage(content=human))
+#         history_langchain_format.append(AIMessage(content=ai))
+#     history_langchain_format.append(HumanMessage(content=message))
+#     gpt_response = getAnswer(message)
+#     return gpt_response
+
+
+# chatbot = gr.Chatbot(  # uploaded image of user and cBioportal as avatar 
+#     [],
+#     elem_id="chatbot",
+#     bubble_full_width=False,
+#     avatar_images=("demo/sample_data/user_avatar.png", 
+#                    "demo/sample_data/chatbot_avatar.png"),
+# )
+
+# gr.ChatInterface(predict, title="cBioPortal Mbox ChatBot", chatbot=chatbot).launch()
--- demo/mbox/mbox_to_json.py
+++ demo/mbox/mbox_to_json.py
@@ -1,3 +1,86 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:01e3a61233928e5c90726dd587e01afc494bc90e6a68ce786c8d45ec4d2364fb
-size 3238
+import mailbox
+import bs4
+import json
+
+
+def get_html_text(html):
+    try:
+        return bs4.BeautifulSoup(html, 'lxml').body.get_text(' ', strip=True)
+    except AttributeError:  # message contents empty
+        return None
+
+
+class GmailMboxMessage():
+    def __init__(self, email_data):
+        if not isinstance(email_data, mailbox.mboxMessage):
+            raise TypeError('Variable must be type mailbox.mboxMessage')
+        self.email_data = email_data
+
+    def parse_email(self):
+        email_labels = self.email_data['X-Gmail-Labels']
+        email_date = self.email_data['Date']
+        email_from = self.email_data['From']
+        email_to = self.email_data['To']
+        email_subject = self.email_data['Subject']
+        email_text = self.read_email_payload() 
+
+        self.email_from = self.email_data['From']
+        email_to = self.email_data['To']
+        self.email_to = self.email_data['To']
+        email_subject = self.email_data['Subject']
+        self.email_subject = self.email_data['Subject']
+        email_text = self.read_email_payload() 
+        self.email_text = self.read_email_payload()
+
+    def read_email_payload(self):
+        email_payload = self.email_data.get_payload()
+        if self.email_data.is_multipart():
+            email_messages = list(self._get_email_messages(email_payload))
+        else:
+            email_messages = [email_payload]
+        return [self._read_email_text(msg) for msg in email_messages]
+
+    def _get_email_messages(self, email_payload):
+        for msg in email_payload:
+            if isinstance(msg, (list, tuple)):
+                for submsg in self._get_email_messages(msg):
+                    yield submsg
+            elif msg.is_multipart():
+                for submsg in self._get_email_messages(msg.get_payload()):
+                    yield submsg
+            else:
+                yield msg
+
+    def _read_email_text(self, msg):
+        content_type = 'NA' if isinstance(msg, str) else msg.get_content_type()
+        encoding = 'NA' if isinstance(msg, str) else msg.get('Content-Transfer-Encoding', 'NA')
+        if 'text/plain' in content_type and 'base64' not in encoding:
+            msg_text = msg.get_payload()
+        elif 'text/html' in content_type and 'base64' not in encoding:
+            msg_text = get_html_text(msg.get_payload())
+        elif content_type == 'NA':
+            msg_text = get_html_text(msg)
+        else:
+            msg_text = None
+        return (content_type, encoding, msg_text)
+
+
+######################### End of library, example of use below
+
+mbox_obj = mailbox.mbox('langchain_gsoc_main/cBioportal_data/documentation_site/mbox/google_conversations.mbox')
+
+num_entries = len(mbox_obj)
+
+with open("mbox_meassage.json", "w") as file:
+    output = []
+    for idx, email_obj in enumerate(mbox_obj):
+        email_data = GmailMboxMessage(email_obj)
+        email_data.parse_email()
+        dict = {}
+        dict['email from'] = str(email_data.email_from)
+        dict['email to'] = str(email_data.email_to)
+        dict['email subject'] = str(email_data.email_subject)
+        dict['email text'] = str(email_data.email_text)
+        output.append(dict)
+    json_object = json.dumps(output, indent=4)
+    file.write(json_object)
