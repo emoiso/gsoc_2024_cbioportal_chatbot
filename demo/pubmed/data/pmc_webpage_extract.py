89a55ab4e18a9c3a54aebfd5b569626dd21f7c02
XinlingWang0628 <105673895+XinlingWang0628@users.noreply.github.com>
2024-07-22 14:38:45 -0400

    update pubmed

--- demo/pubmed/data/pmc_webpage_extract.py
+++ demo/pubmed/data/pmc_webpage_extract.py
@@ -1,3 +1,25 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:760d788d0e149e1230ad8d57191507213bf1fabd022a0f98329dad644c3dc9a6
-size 745
+import requests, lxml
+from bs4 import BeautifulSoup
+
+headers = {
+    'User-agent':
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36"
+}
+
+url = 'https://www.ncbi.nlm.nih.gov/pmc/articles/PMC6419906/'
+url_cBio = 'https://www.ncbi.nlm.nih.gov/pmc/articles/PMC4048021/'
+soup = BeautifulSoup(requests.get(url_cBio, headers=headers).text, 'lxml')
+
+body = soup.body
+with open('pubmed_webfile_body.txt', 'w') as file:
+    file.write(str(body))
+
+for tag in body.select('script'):
+    tag.decompose()
+for tag in body.select('style'):
+    tag.decompose()
+
+text = body.get_text(separator='\n').strip()
+# with open('pubmed_webfile.txt', 'w') as file:
+#     file.write(text)
+print(text)
--- demo/pubmed/data/test_db.py
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:8242634c2d8efd447b113e5807ef79076a6e613eb5fd1737957d9c332ea5420e
-size 590
